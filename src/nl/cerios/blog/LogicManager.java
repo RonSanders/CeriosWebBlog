123f2e0e5ce3022c720edaa7e4fcd9e58904c929
Rutger van Velzen <rutger.van.velzen@cerios.nl>
2015-11-17 14:54:11 +0100

    changed some coppelings but no all!

--- src/nl/cerios/blog/LogicManager.java
+++ src/nl/cerios/blog/LogicManager.java
@@ -1,6 +1,5 @@
 package nl.cerios.blog;
 import java.util.List;
-import nl.cerios.blog.UserInterfaceManager.CurrentScreen;
 import nl.cerios.blog.model.Message;
 import nl.cerios.blog.model.NewUserRequest;
 import nl.cerios.blog.model.User;
@@ -12,18 +11,12 @@ public class LogicManager {
 	public static String getCurrentLoggedinUserName() {
 		return currentLoggedinUser.getUsername();
 	}
-
-	public static void main(String[] args) {
-		UserInterfaceManager.switchCurrentScreen(CurrentScreen.SHOW_SCREEN_WELCOME);
-	}
-	
 	public static void signIn(UserIdentificationRequest uir){
 		// get the matching user out of the database 
 		// and store it local as the current user.
 		currentLoggedinUser = DatabaseManager.getUser(uir);
 		// Do sign in correct stuff
 		Debug.log(currentLoggedinUser.getUsername());
-		UserInterfaceManager.switchCurrentScreen(CurrentScreen.SHOW_SCREEN_SIGN_IN_CORRECT);
 		// Or do sign in NOT correct stuff.
 	}
 	
@@ -31,9 +24,7 @@ public class LogicManager {
 		// if go is true, do sign out stuff
 		if(signOut){
 			currentLoggedinUser = null;
-			UserInterfaceManager.switchCurrentScreen(CurrentScreen.SHOW_SCREEN_SIGN_OUT_CORRECT);
 		}else{
-			UserInterfaceManager.switchCurrentScreen(CurrentScreen.SHOW_SCREEN_SIGN_OUT_NOT_CORRECT);
 		}
 		// else do sign out no correct stuff
 	}
@@ -42,13 +33,11 @@ public class LogicManager {
 		// Do sign up correct stuff
 		DatabaseManager.newUser(newUserRequest);
 		currentLoggedinUser = DatabaseManager.newUser(newUserRequest);
-		UserInterfaceManager.switchCurrentScreen(CurrentScreen.SHOW_SCREEN_SIGN_UP_CORRECT);
 		// or do sign up not correct stuff
 	}
 	
 	public static void AddNewMessage(Message newMessage){
 		DatabaseManager.newMessage(newMessage);
-		UserInterfaceManager.switchCurrentScreen(CurrentScreen.SHOW_SCREEN_BLOG_NAVIGATION);
 	}
 	
 	public static List<Message> getAllMessages(){
--- src/nl/cerios/blog/UserInterfaceManager.java
+++ src/nl/cerios/blog/UserInterfaceManager.java
@@ -1,6 +1,57 @@
 package nl.cerios.blog;
+import java.util.Date;
+import java.util.List;
+import nl.cerios.blog.model.Message;
+import nl.cerios.blog.model.NewUserRequest;
+import nl.cerios.blog.model.UserIdentificationRequest;
 
+/**
+ * <h1>UserInterfaceManager</h1>
+ * <b>This is a collection of all the user interface screens</b><br>
+ * <ul>
+ * <li>mainMenu</li>
+ * <li>blogMenu</li>
+ * <li>guestMenu</li>
+ * </ul>
+ * @author	Rutger van Velzen, Ron Sanders and Marcel Groothuis
+ * @version 0.1.0
+ * @since	01-11-2015
+ */
+
+//////////////////// To Do ////////////////////
+//
+//	-Maybe Correct and not-correct should be 
+//		an status enum, that adds to the
+//		current screen enum (think about it
+//		as a sub screen).
+//	
+//	-Other option, add a value to the enum
+//		that indicates the status.
+//
+//	-Guest should be the same as a normal
+//		user but restricted if no one is 
+//		logged in.
+//
+//	-Work out a select post by title option
+//		show the actor a list of titles not
+//		the whole content of all the messages
+//
+//	-Make a surge option to find posts by
+//		an input string matching a (part of a)
+//		title.
+//
+//	-Order the read posts list buy date.
+//
+//	-Show the user an option to show all titles
+//		up to a certain date. Think of :
+//		"today, this week, this month"
+//
+//////////////////////////////////////////////
 public class UserInterfaceManager {
+	public static void main(String[] args) {
+		UserInterfaceManager.switchCurrentScreen(CurrentScreen.SHOW_SCREEN_WELCOME);
+	}
+	
 	public enum CurrentScreen {
 		SHOW_SCREEN_WELCOME, 
 		SHOW_SCREEN_BLOG_NAVIGATION, 
@@ -18,64 +69,209 @@ public class UserInterfaceManager {
 		SHOW_SCREEN_SHOW_MESSAGES,
 		SHOW_SCREEN_GET_ALL_MESSAGES
 	}
-	
 	private static CurrentScreen currentScreen = CurrentScreen.SHOW_SCREEN_WELCOME;
 	public static CurrentScreen getCurrentScreen() {
 		return currentScreen;
 	}
-
 	public static void switchCurrentScreen(CurrentScreen currentScreen) {
 		UserInterfaceManager.currentScreen = currentScreen;
 	
 		switch(getCurrentScreen()){
 			case SHOW_SCREEN_WELCOME :
-				UserInterfaces.showScreen_Welcome();
+				showScreen_Welcome();
 				break;
 			case SHOW_SCREEN_BLOG_NAVIGATION :
-				UserInterfaces.showScreen_BlogNavigation(LogicManager.getCurrentLoggedinUserName());
+				showScreen_BlogNavigation(LogicManager.getCurrentLoggedinUserName());
 				break;
 			case SHOW_SCREEN_GUEST :
-				UserInterfaces.showScreen_Guest();
+				showScreen_Guest();
 				break;
 		
 			case SHOW_SCREEN_SIGN_IN :
-				LogicManager.signIn(UserInterfaces.showScreen_SignIn());
+				showScreen_SignIn();
 				break;
 			case SHOW_SCREEN_SIGN_IN_CORRECT :
-				UserInterfaces.showScreen_SignIn_Correct();
+				showScreen_SignIn_Correct();
 				break;
 			case SHOW_SCREEN_SIGN_IN_NOT_CORRECT :
-				UserInterfaces.showScreen_SignIn_NotCorrect();
+				showScreen_SignIn_NotCorrect();
 				break;
 			
 			case SHOW_SCREEN_SIGN_OUT :
-				LogicManager.signOut(UserInterfaces.showScreen_SignOut());
+				showScreen_SignOut();
 				break;
 			case SHOW_SCREEN_SIGN_OUT_CORRECT :
-				UserInterfaces.showScreen_SignOut_Correct();
+				showScreen_SignOut_Correct();
 				break;
 			case SHOW_SCREEN_SIGN_OUT_NOT_CORRECT :
-				UserInterfaces.showScreen_SignOut_NotCorrect();
+				showScreen_SignOut_NotCorrect();
 				break;
 		
 			case SHOW_SCREEN_SIGN_UP :
-				LogicManager.signUp(UserInterfaces.showScreen_SignUp());
+				showScreen_SignUp();
 				break;
 			case SHOW_SCREEN_SIGN_UP_CORRECT :
-				UserInterfaces.showScreen_SignUp_Correct();
+				showScreen_SignUp_Correct();
 				break;
 			case SHOW_SCREEN_SIGN_UP_NOT_CORRECT :
-				UserInterfaces.showScreen_SignUp_NotCorrect();
+				showScreen_SignUp_NotCorrect();
 				break;
 	
 			case SHOW_SCREEN_NEW_MESSAGES :
-				LogicManager.AddNewMessage(UserInterfaces.showScreen_NewMessage());
+				showScreen_NewMessage();
 				break;
 			case SHOW_SCREEN_GET_ALL_MESSAGES :
-				UserInterfaces.showScreen_ShowMessages(LogicManager.getAllMessages());
+				showScreen_ShowMessages(LogicManager.getAllMessages());
 				break;
 			default:
-				UserInterfaces.showScreen_Welcome();
+				showScreen_Welcome();
 		}
+	}
+	public static void showScreen_Welcome(){
+		int index = Input.intInput(
+				"Welcome to Cerios blog!\n"+
+				"1) Sign in?\n"+
+				"2) Sign up?\n"+
+				"Type a number");
+		switch(index){
+		case 1 :
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_SIGN_IN);
+			break;
+		case 2 :
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_SIGN_UP);
+			break;
+		default:
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_WELCOME);
+		}
+	}
+	public static void showScreen_BlogNavigation(String name){
+		int index = Input.intInput(
+				"Welcome "+name+"!\n"+ 
+				"1) Do you want to write a new post?\n"+
+				"2) Do you want to read the blog posts?\n"+
+				"3) Do you want to signout?\n");
+		switch(index){
+		case 1 :
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_NEW_MESSAGES);
+			break;
+		case 2 :
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_GET_ALL_MESSAGES);
+			break;
+		case 3 :
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_SIGN_OUT);
+			break;
+		default:
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_BLOG_NAVIGATION);
+		}
+	}
+	
+	public static void showScreen_Guest(){ // must be one with showScreen_BlogNavigation, contend based on current user == null?
+		int index = Input.intInput(
+				"Welkom Guest\n"+
+				"1) Do you want to read all posts?\n"+
+				"2) Do you want to sign out?");
+		switch(index){
+		case 1 :
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_GET_ALL_MESSAGES);
+			break;
+		case 2 :
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_SIGN_OUT);
+			break;
+		default:
+			switchCurrentScreen(CurrentScreen.SHOW_SCREEN_GUEST);
+		}
+	}
+	
+	////////////////////
+	//
+	//		Sign in
+	//
+	////////////////////
+	public static UserIdentificationRequest showScreen_SignIn(){
+		UserIdentificationRequest uir = new UserIdentificationRequest();
+		uir.setUsername(Input.stringInput("Enter your userName."));
+		uir.setPassword(Input.stringInput("Enter your password."));
+		
+		return uir;
+	}
+	
+	public static void showScreen_SignIn_Correct(){
+		System.out.println("You are signed in!");
+		switchCurrentScreen(CurrentScreen.SHOW_SCREEN_BLOG_NAVIGATION);
+	}
+	
+	public static void showScreen_SignIn_NotCorrect(){
+		System.out.println("You are NOT logged in!");
+		switchCurrentScreen(CurrentScreen.SHOW_SCREEN_WELCOME);
+	}
+	
+	////////////////////
+	//
+	//		Sign out
+	//
+	////////////////////
+	public static boolean showScreen_SignOut(){
+		return Input.yes("Are You sure, you want to sign out?(y/n)");
+	}
+	
+	public static void showScreen_SignOut_Correct(){
+		System.out.println("You are signed out!\n Good Bye!\n");
+		switchCurrentScreen(CurrentScreen.SHOW_SCREEN_WELCOME);
+	}
+	
+	//showScreen_SignOut_NotCorrect
+	public static void showScreen_SignOut_NotCorrect(){
+		System.out.println("Oops!\n We cant sign you out right now.\n");
+		switchCurrentScreen(CurrentScreen.SHOW_SCREEN_BLOG_NAVIGATION);
+	}
+	
+	////////////////////
+	//
+	//		Sign up
+	//
+	////////////////////
+	public static void showScreen_SignUp(){
+		NewUserRequest ur = new NewUserRequest();
+		ur.setUsername(Input.stringInput("Enter your username here:"));
+		
+		String tempSavePassword = Input.stringInput("Enter your password here:");
+		if(tempSavePassword.equals(Input.stringInput("Enter your password again here:")))
+			ur.setPassword(tempSavePassword);
+		else
+			System.out.println("Your password did not match!");
+		switchCurrentScreen(CurrentScreen.SHOW_SCREEN_WELCOME);
+	}
+	public static void showScreen_SignUp_Correct(){
+		System.out.println("You are signed up!\n");
+		switchCurrentScreen(CurrentScreen.SHOW_SCREEN_BLOG_NAVIGATION);
+	}
+	public static void showScreen_SignUp_NotCorrect(){
+		System.out.println("Oops!\n We cant sign you up right now.\n");
+	}
+	
+	////////////////////
+	//
+	//		post
+	//
+	////////////////////
+	
+	public static Message showScreen_NewMessage(){
+		Message message = new Message();
+		message.setTitle(Input.stringInput("Enter your Title:"));
+		message.setBody(Input.stringInput("Enter your text:"));
+		message.setDate(new Date());
+		
+		return message;
+	}
+	
+	
+	public static void showScreen_ShowMessages(List<Message> messages){
+		for (Message message : messages) {
+			System.out.println(message.getTitle());
+			System.out.println(message.getBody());
+			System.out.println();
+		}
+		
+		switchCurrentScreen(CurrentScreen.SHOW_SCREEN_BLOG_NAVIGATION);
 	}
 }
