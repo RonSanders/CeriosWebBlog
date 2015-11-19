7f1bb5db4a755a251b6e3d8c9aa1e47d2fa0bcf3
Rutger van Velzen <rutger.van.velzen@cerios.nl>
2015-11-19 13:46:31 +0100

    woop

--- src/nl/cerios/blog/DatabaseManager.java
+++ src/nl/cerios/blog/DatabaseManager.java
@@ -6,8 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList;import java.util.List;
 import java.util.Properties;
 
 import nl.cerios.blog.model.Message;
